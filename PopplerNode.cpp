3d8f416e4f00d17f749794f6382bf5507f0e621b
Hendrik Sollich <Hendrik@hoodie.de>
2014-08-27 17:07:28 +0200

    Initial commit

---
+++ PopplerNode.cpp
@@ -0,0 +1,239 @@
+#include <base/Logger.h>
+#include <base/Exception.h>
+#include <player/OGLSurface.h>
+#include <graphics/GLContextManager.h>
+#include <graphics/BitmapLoader.h>
+#include <graphics/OGLHelper.h>
+#include <wrapper/WrapHelper.h>
+#include <wrapper/raw_constructor.hpp>
+
+
+#include <string>
+#include <iostream>
+#include <glib/poppler.h>
+#include <cairo-pdf.h>
+
+#include "PopplerNode.h"
+
+using namespace std;
+using namespace boost::python;
+using namespace avg;
+
+char popplerNodeName[] = "popplernode";
+
+void PopplerNode::
+registerType()
+{
+    TypeDefinition
+    def = TypeDefinition("popplernode", "rasternode",
+                         ExportedObject::buildObject<PopplerNode>)
+          .addArg( Arg<std::string>("path","",false,offsetof(PopplerNode,m_pPdfPath)) ) ;
+    //.addArg(  Arg<string>("fillcolor",   "0F0F0F",  false,  offsetof(ColorNode,  m_sFillColorName) ));
+
+    //const char* allowedParentNodeNames[] = {"avg", 0};
+    const char* allowedParentNodeNames[] = {"avg", "div",  0};
+    //TypeRegistry::get()->registerType(def);
+    TypeRegistry::get()->registerType(def, allowedParentNodeNames);
+}
+
+PopplerNode:: ~PopplerNode() {
+
+}
+
+PopplerNode::
+PopplerNode(const ArgList& args)
+    : m_pPdfPath("")
+    , m_pPixelFormat(avg::B8G8R8X8)
+    , m_bNewBmp(false)
+{
+    AVG_TRACE(
+        Logger::category::PLUGIN,
+        Logger::severity::INFO,
+        "PopplerNode c'tor gets Argument path= "  << args.getArgVal<string>("path")
+    );
+
+    args.setMembers(this);
+
+    AVG_TRACE(
+        Logger::category::PLUGIN,
+        Logger::severity::INFO,
+        "PopplerNode constructed with " << m_pPdfPath // TODO replace
+    );
+
+    
+    if(this->loadDocument()) {
+    }
+    else {
+      cout << "[fail] could not open document" << endl;
+        // TODO load some placeholder in case of loadfailure
+    }
+
+}
+
+void
+PopplerNode::setPath(std::string path) {
+    std::cout << "setting path to \"" << path << "\"" << std::endl;
+    m_pPdfPath = path;
+}
+
+const string
+PopplerNode::
+getPopplerVersion() const {
+    return poppler_get_version();
+}
+
+IntPoint PopplerNode::getMediaSize()
+{
+  // TODO store a currentPage
+  PopplerPage *page = poppler_document_get_page( m_document, 0);
+  double width, height;
+  poppler_page_get_size(page, &width, &height);
+  return IntPoint(width,height);
+}
+
+const string
+PopplerNode::
+getPath() const {
+    return m_pPdfPath;
+}
+
+bool
+PopplerNode::
+loadDocument()
+{
+    cout << "--- loading document (" << m_pPdfPath << ")" << endl;
+    GError *error = NULL;
+    m_document = poppler_document_new_from_file(m_pPdfPath.c_str(), NULL, &error);
+
+    if(m_document == NULL) {
+        cout << "[fail] Problem loading " << m_pPdfPath << endl;
+        cout << error->message << endl;
+        return false;
+    }
+    cout << "[ok] loaded document " << poppler_document_get_title(m_document) << endl;
+    m_pageCount = poppler_document_get_n_pages(m_document);
+    return true;
+}
+
+
+void PopplerNode::
+fill_bitmap(PopplerPage *page)
+{
+  //cout << "--- fill_bitmap()" << endl;
+  cairo_surface_t *surface;
+  cairo_t *cairo;
+  //double xscale, yscale;
+  
+  IntPoint size = getMediaSize();
+  
+  surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, size.x, size.y);
+  cairo   = cairo_create(surface);
+  
+  //cout << "---- created surface" << endl;
+  poppler_page_render( page, cairo );
+  cairo_set_operator(cairo, CAIRO_OPERATOR_DEST_OVER);
+  cairo_set_source_rgb(cairo, 1., 1., 1.);
+  cairo_paint(cairo);
+  
+  //cout << "---- rendered page" << endl;
+  //cairo_surface_flush(surface);
+  unsigned char* data = cairo_image_surface_get_data(surface);
+  int stride          = cairo_image_surface_get_stride(surface);
+  m_pBitmap           = avg::BitmapPtr(
+    new avg::Bitmap(size, m_pPixelFormat, data, stride, true)
+  );
+  m_bNewBmp           = true;
+  
+  //cout << "---- dumped bitmap" << endl;
+  //clean up afterwards
+  cairo_destroy(cairo);
+  cairo_surface_destroy(surface);
+  //cout << "---- cleaned up surface" << endl;
+}
+
+void PopplerNode:: open()
+{
+    //cout << "+++ open()" << endl;
+    
+    setViewport(-32767, -32767, -32767, -32767);
+    PopplerPage *page = poppler_document_get_page(m_document, 0);
+    IntPoint     size = getMediaSize();
+    bool bMipmap = getMaterial().getUseMipmaps();
+
+    m_pTex    = GLContextManager::get()->createTexture(size, m_pPixelFormat, bMipmap);
+    getSurface()->create(m_pPixelFormat,m_pTex);
+    fill_bitmap(page);
+    newSurface();
+    setupFX();
+    //cout << "++++ set bitmap" << endl;
+}
+
+void PopplerNode:: connect(CanvasPtr pCanvas)
+{
+  //cout << "... connect()" << endl;
+  RasterNode::connect(pCanvas);
+}
+
+void PopplerNode::connectDisplay()
+{
+  //cout << "... connectDisplay()" << endl;
+  RasterNode::connectDisplay();
+  open();
+}
+
+void PopplerNode:: preRender(const VertexArrayPtr& pVA, bool bIsParentActive, float parentEffectiveOpacity)
+{
+  Node::preRender(pVA, bIsParentActive, parentEffectiveOpacity);
+  //cout << "... preRender()" << endl;
+  //PopplerPage* page = poppler_document_get_page(m_document, 0);
+  if(isVisible()) {
+    if(m_bNewBmp) {
+      //ScopeTime Timer(); # TODO time poppler rendering
+      GLContextManager::get()->scheduleTexUpload(m_pTex, m_pBitmap);
+      m_bNewBmp = false;
+    }
+  }
+  calcVertexArray(pVA);
+}
+
+void PopplerNode:: renderFX() {
+    RasterNode::renderFX(getSize(), Pixel32(255, 255, 255, 255), false);
+}
+
+void PopplerNode:: render() {
+  //cout << "... render()" << endl;
+  //ScopeTimer Timer(CameraProfilingZone);
+  blt32(getTransform(), getSize(), getEffectiveOpacity(), getBlendMode());
+}
+
+
+
+
+
+BOOST_PYTHON_MODULE(popplernode) {
+    class_<PopplerNode, bases<RasterNode>, boost::noncopyable>("PopplerNode", no_init)
+
+    .def( "__init__", raw_constructor(createNode<popplerNodeName>) )
+
+    .add_property( "path",
+                   &PopplerNode::getPath,
+                   &PopplerNode::setPath
+                 )
+    .add_property( "poppler_version",
+                   &PopplerNode::getPopplerVersion
+                 );
+
+    //.add_property( "fillcolor",
+    //    make_function(&ColorNode::getFillColor, return_value_policy<copy_const_reference>()),
+    //    &ColorNode::setFillColor);
+}
+
+AVG_PLUGIN_API void registerPlugin() {
+    initpopplernode();
+    object mainModule(handle<>(borrowed(PyImport_AddModule("__builtin__"))));
+    object popplerModule(handle<>(PyImport_ImportModule("popplernode")));
+    mainModule.attr("popplerplugin") = popplerModule;
+
+    avg::PopplerNode::registerType();
+}
+
---
+++ PopplerNode.h
@@ -0,0 +1,69 @@
+//  libavg - Media Playback Engine.
+//  Copyright (C) 2003-2014 Ulrich von Zadow
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  Current versions can be found at www.libavg.de
+//
+//  Original author of this file is Hendrik Sollich <hendrik@hoodie.de>
+//
+
+#include <api.h>
+
+#include <player/Player.h>
+#include <player/RasterNode.h>
+#include <player/TypeDefinition.h>
+
+#include <graphics/MCTexture.h>
+
+
+using namespace std;
+using namespace boost::python;
+
+namespace avg {
+
+class AVG_API PopplerNode : public RasterNode
+{
+public:
+    static void registerType();
+    PopplerNode(const ArgList& args);
+    virtual ~PopplerNode();
+
+    const string  getPath() const;
+            void  setPath(std::string path);
+    const string  getPopplerVersion() const;
+            bool  loadDocument();
+    virtual void  open();
+            void  fill_bitmap(PopplerPage *page);
+    virtual void  renderFX();
+    virtual void  render();
+    virtual void  preRender(const VertexArrayPtr& pVA, bool bIsParentActive, float parentEffectiveOpacity);
+    virtual void  connect(CanvasPtr pCanvas);
+    virtual void  connectDisplay();
+        IntPoint  getMediaSize();
+    
+
+private:
+        std::string  m_pPdfPath;
+       MCTexturePtr  m_pTex;
+          BitmapPtr  m_pBitmap;
+   avg::PixelFormat  m_pPixelFormat;
+               bool  m_bNewBmp;
+    PopplerDocument *m_document;
+                int  m_pageCount;
+};
+
+
+}
