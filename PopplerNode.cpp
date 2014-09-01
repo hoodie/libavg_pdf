10d7858f886e8240349626cbb09729311eae5423
Hendrik Sollich <Hendrik@hoodie.de>
2014-09-01 17:54:45 +0200

    reformated PopplerNode.cpp and plugin.cpp

--- PopplerNode.cpp
+++ PopplerNode.cpp
@@ -21,7 +21,8 @@ namespace py = boost::python;
 
 typedef   int page_index_t ;
   
-void PopplerNode::
+void
+PopplerNode::
 registerType()
 {
     TypeDefinition
@@ -39,8 +40,7 @@ registerType()
 PopplerNode:: PopplerNode() { }
 PopplerNode:: ~PopplerNode() { }
 
-PopplerNode::
-PopplerNode(const ArgList& args)
+PopplerNode::PopplerNode(const ArgList& args)
     : m_pPixelFormat(avg::B8G8R8A8)
     , m_pPdfPath("")
     , m_bNewSize(false)
@@ -48,7 +48,6 @@ PopplerNode(const ArgList& args)
     , m_iPageCount(-1)
     , m_iCurrentPage(-1)
 {
-  
   AVG_TRACE( Logger::category::PLUGIN, Logger::severity::INFO, "PopplerNode c'tor gets Argument path= "  << args.getArgVal<string>("path") );
   AVG_TRACE( Logger::category::PLUGIN, Logger::severity::INFO, "PopplerNode constructed with " << m_pPdfPath );
   args.setMembers(this);
@@ -64,8 +63,8 @@ void
 PopplerNode::
 setPath(std::string path)
 {
-    std::cout << "setting path to \"" << path << "\"" << std::endl;
-    m_pPdfPath = path;
+  std::cout << "setting path to \"" << path << "\"" << std::endl;
+  m_pPdfPath = path;
 }
 
 
@@ -83,7 +82,9 @@ getPopplerVersion() const
   return poppler_get_version();
 }
 
-const int PopplerNode::getPageCount() const
+const int
+PopplerNode::
+getPageCount() const
 {
   return m_iPageCount;
 }
@@ -151,48 +152,51 @@ bool
 PopplerNode::
 loadDocument()
 {
-    //cout << "--- loading document (" << m_pPdfPath << ")" << endl;
-    GError *error = NULL;
-    m_pDocument = poppler_document_new_from_file(m_pPdfPath.c_str(), NULL, &error);
-
-    if(m_pDocument == NULL) {
-        //cout << "[fail] Problem loading " << m_pPdfPath << endl;
-        //cout << error->message << endl;
-        return false;
-    }
-    //cout << "[ok] loaded document " << poppler_document_get_title(m_pDocument) << endl;
-    m_iPageCount = poppler_document_get_n_pages(m_pDocument);
+  //cout << "--- loading document (" << m_pPdfPath << ")" << endl;
+  GError *error = NULL;
+  m_pDocument = poppler_document_new_from_file(m_pPdfPath.c_str(), NULL, &error);
+  
+  if(m_pDocument == NULL) {
+    //cout << "[fail] Problem loading " << m_pPdfPath << endl;
+    //cout << error->message << endl;
+    return false;
+  }
+  //cout << "[ok] loaded document " << poppler_document_get_title(m_pDocument) << endl;
+  m_iPageCount = poppler_document_get_n_pages(m_pDocument);
+  
+  if(m_iPageCount > 0){
+    m_vPages = std::vector<PopplerPage*> (m_iPageCount);
+    m_vPageBitmaps = std::vector<avg::BitmapPtr>(m_iPageCount);
     
-    if(m_iPageCount > 0){
-      m_vPages = std::vector<PopplerPage*> (m_iPageCount);
-      m_vPageBitmaps = std::vector<avg::BitmapPtr>(m_iPageCount);
-      
-      for(int i = 0; i< m_iPageCount; ++i){
-        m_vPages.at(i) = (poppler_document_get_page(m_pDocument,i));
-      }
-      setCurrentPage(0);
-      
+    for(int i = 0; i< m_iPageCount; ++i){
+      m_vPages.at(i) = (poppler_document_get_page(m_pDocument,i));
     }
-    else
-      cout << "document seems to have 0 pages";
-      return false;
-
-    return true;
+    setCurrentPage(0);
+    
+  }
+  else
+    cout << "document seems to have 0 pages";
+  return false;
+  
+  return true;
 }
 
-void PopplerNode::setCurrentPage(page_index_t page_index)
+void
+PopplerNode::
+setCurrentPage(page_index_t page_index)
 {
-      m_iCurrentPage = page_index;
-      m_pNodeSize = getPageSize(page_index);
+  m_iCurrentPage = page_index;
+  m_pNodeSize = getPageSize(page_index);
 }
 
-void PopplerNode
+void
+PopplerNode
 ::fill_bitmap(PopplerPage *page, double width = 0, double height= 0)
 {
   std::clog << "--- fill_bitmap()" << endl;
   cairo_surface_t *surface;
   cairo_t *cairo;
-  
+
   IntPoint size = IntPoint(width, height);
   if (width == 0 or height==0)
     size = m_pNodeSize;
@@ -201,26 +205,26 @@ void PopplerNode
     m_bNewSize  = true;
     std::clog << "setting new size: " << width << " ," << height << endl;
   }
-    
-  
+
+
   double xscale, yscale;
   xscale = size.x / (double)getPageSize(page).x;
   yscale = size.y / (double)getPageSize(page).y;
-  
+
   std::clog << "pagesize to:  " << getPageSize(m_iCurrentPage).x << " ," << getPageSize(m_iCurrentPage).y << endl;
   std::clog << "scaling to:  " << xscale << " ," << yscale << endl;
-  
+
   surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, size.x, size.y);
   cairo   = cairo_create(surface);
-  
+
   cairo_scale(cairo, xscale,yscale);
-  
+
   poppler_page_render( page, cairo );
-  
+
   cairo_set_operator(cairo, CAIRO_OPERATOR_DEST_OVER);
   cairo_set_source_rgba(cairo, 1., 0., 0., 0.);
   cairo_paint(cairo);
-  
+
   cairo_surface_flush(surface);
   unsigned char* data = cairo_image_surface_get_data(surface);
   int stride          = cairo_image_surface_get_stride(surface);
@@ -228,17 +232,21 @@ void PopplerNode
     new avg::Bitmap(size, m_pPixelFormat, data, stride, true)
   );
   m_bNewBmp           = true;
-  
+
   cairo_destroy(cairo);
   cairo_surface_destroy(surface);
 }
 
-void PopplerNode
-::rerender(page_index_t page_index)
-{resize(page_index, 0,0);}
+void
+PopplerNode::
+rerender(page_index_t page_index)
+{
+  resize(page_index, 0,0);
+}
 
-void PopplerNode
-::resize(page_index_t page_index, double width = 0, double height = 0)
+void
+PopplerNode::
+resize(page_index_t page_index, double width = 0, double height = 0)
 {
   if(page_index < 0 or page_index >= m_iPageCount)
     return;
@@ -250,49 +258,51 @@ void PopplerNode
   //cout << endl;
   
   cout << "resizing to " << width << ", " << height;
-  
+
   fill_bitmap(page, width, height);
-  
+
 }
 
-void PopplerNode:: open()
+void
+PopplerNode::
+open()
 {
-    //cout << "+++ open()" << endl;
-    
-    setViewport(-32767, -32767, -32767, -32767);
-    setupContext();
-    PopplerPage *page = poppler_document_get_page(m_pDocument, 0);
-    fill_bitmap(page);
+  setViewport(-32767, -32767, -32767, -32767);
+  setupContext();
+  PopplerPage *page = poppler_document_get_page(m_pDocument, 0);
+  fill_bitmap(page);
 }
 
-void PopplerNode::setupContext(){
-
-    bool bMipmap = getMaterial().getUseMipmaps();
-    m_pTex    = GLContextManager::get()->createTexture(m_pNodeSize, m_pPixelFormat, bMipmap);
-    getSurface()->create(m_pPixelFormat,m_pTex);
-    newSurface();
-    setupFX();
-    //cout << "++++ set bitmap" << endl;
+void
+PopplerNode::
+setupContext(){
+  bool bMipmap = getMaterial().getUseMipmaps();
+  m_pTex    = GLContextManager::get()->createTexture(m_pNodeSize, m_pPixelFormat, bMipmap);
+  getSurface()->create(m_pPixelFormat,m_pTex);
+  newSurface();
+  setupFX();
 }
 
-void PopplerNode:: connect(CanvasPtr pCanvas)
+void
+PopplerNode::
+connect(CanvasPtr pCanvas)
 {
-  //cout << "... connect()" << endl;
   RasterNode::connect(pCanvas);
 }
 
-void PopplerNode::connectDisplay()
+void
+PopplerNode::
+connectDisplay()
 {
-  //cout << "... connectDisplay()" << endl;
   RasterNode::connectDisplay();
   open();
 }
 
-void PopplerNode:: preRender(const VertexArrayPtr& pVA, bool bIsParentActive, float parentEffectiveOpacity)
+void
+PopplerNode::
+preRender(const VertexArrayPtr& pVA, bool bIsParentActive, float parentEffectiveOpacity)
 {
   Node::preRender(pVA, bIsParentActive, parentEffectiveOpacity);
-  //cout << "... preRender()" << endl;
-  //PopplerPage* page = poppler_document_get_page(m_document, 0);
   if(isVisible()) {
     if(m_bNewSize) {
       setupContext();
@@ -308,7 +318,9 @@ void PopplerNode:: preRender(const VertexArrayPtr& pVA, bool bIsParentActive, fl
   calcVertexArray(pVA);
 }
 
-void PopplerNode:: renderFX() {
+void
+PopplerNode::
+renderFX() {
     RasterNode::renderFX(getSize(), Pixel32(255, 255, 255, 255), false);
 }
 
@@ -318,14 +330,13 @@ void PopplerNode:: render() {
   blt32(getTransform(), getSize(), getEffectiveOpacity(), getBlendMode());
 }
 
-void PopplerNode:: testFunction(){
-  // implements all sorts of test stuff
+void
+PopplerNode::
+testFunction(){
   cout << "---testfunction--x" << endl;
   
-  //cout << AreaNode::getSize().x <<  ", " << AreaNode::getSize().y << endl;
   resize(0, AreaNode::getSize().x, AreaNode::getSize().y);
   cout << m_pNodeSize << endl;
-  
   
   cout << "---testfunction---" << endl << endl;
 }
--- plugin.cpp
+++ plugin.cpp
@@ -12,21 +12,21 @@ char pageNodeName[]    = "pagenode";
 
 BOOST_PYTHON_MODULE(popplerplugin) {
   
-    class_<PopplerRectangle>("PopplerRectangle")
-      .add_property("x1", &PopplerRectangle::x1).add_property("x2", &PopplerRectangle::x2)
-      .add_property("y1", &PopplerRectangle::y1).add_property("y2", &PopplerRectangle::y2);
+  class_<PopplerRectangle>("PopplerRectangle")
+    .add_property("x1", &PopplerRectangle::x1).add_property("x2", &PopplerRectangle::x2)
+    .add_property("y1", &PopplerRectangle::y1).add_property("y2", &PopplerRectangle::y2);
       
-    class_<PageNode, bases<RasterNode>, boost::noncopyable>("PageNode", no_init)
+  class_<PageNode, bases<RasterNode>, boost::noncopyable>("PageNode", no_init)
     .def( "__init__", raw_constructor(createNode<pageNodeName>) )
     .add_property( "poppler_version", &PopplerNode::getPopplerVersion );
-    
-    //class_<PopplerAnnot>("PopplerAnnot");
-    class_<PopplerAnnotMapping>("PopplerAnnotMapping")
+  
+  //class_<PopplerAnnot>("PopplerAnnot");
+  class_<PopplerAnnotMapping>("PopplerAnnotMapping")
     .add_property("area",  &PopplerAnnotMapping::area)  
     //.add_property("annot", &PopplerAnnotMapping::annot)  
     ;
-    
-    class_<PopplerNode, bases<RasterNode>, boost::noncopyable>("PopplerNode", no_init)
+  
+  class_<PopplerNode, bases<RasterNode>, boost::noncopyable>("PopplerNode", no_init)
     .def( "__init__", raw_constructor(createNode<popplerNodeName>) )
     .def( "next",        &PopplerNode::getPopplerVersion)
     .def( "test",        &PopplerNode::testFunction)
@@ -36,15 +36,15 @@ BOOST_PYTHON_MODULE(popplerplugin) {
     .def( "annotations", &PopplerNode::getPageAnnotations)
     
     .add_property( "path",
-                   &PopplerNode::getPath,
-                   &PopplerNode::setPath )
+                  &PopplerNode::getPath,
+                  &PopplerNode::setPath )
     .add_property( "mediaSize",       &PopplerNode::getMediaSize )
     .add_property( "pageCount",       &PopplerNode::getPageCount )
     .add_property( "title",           &PopplerNode::getDocumentTitle )
     .add_property( "subject",         &PopplerNode::getDocumentSubject )
     .add_property( "author",          &PopplerNode::getDocumentAuthor )
     .add_property( "poppler_version", &PopplerNode::getPopplerVersion );
-
+    
     //.add_property( "fillcolor",
     //    make_function(&ColorNode::getFillColor, return_value_policy<copy_const_reference>()),
     //    &ColorNode::setFillColor);
